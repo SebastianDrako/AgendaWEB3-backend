0f874b9f9443ef3270de072858970fe19e3ab6d4
SebasitanDrako <sguerrero.sgc@gmail.com>
2022-12-09 15:13:25 -0500

    Se separaron las funciones de verificacion y cuenta de dias biciestos y todo esta listo para empezar a mimplementar los meses

---
+++ Opciones/Basical_operations/Gestion_Del_Tiempo/Calendar.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MZL
+pragma solidity ^0.8.7;
+
+import "./Time.sol";
+
+contract calendar is time {
+
+    uint[12] mesesGregorianos = [31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31 ] ;
+
+
+
+}
--- Opciones/Basical_operations/Gestion_Del_Tiempo/Time.sol
+++ Opciones/Basical_operations/Gestion_Del_Tiempo/Time.sol
@@ -5,7 +5,11 @@ import "../Bool_analisis/Bool.sol";
 
 contract time is bool_analisis {
 
-uint timeZone;
+int timeZone = 0;
+
+event Comptime(uint time, uint timeandZone);
+
+
 
 struct date {
 
@@ -20,10 +24,10 @@ struct date {
 
 
 // Funcion que obtiene Segundos, Minutos, Hora, dias y años segun calendario gregoriano y Fijo internacional
- function fechaGet (int _timeZone) view public returns( uint[5] memory) {
+ function fechaNoFixGet (int _timeZone , uint segundosEpoch) public pure returns( uint[5] memory) {
 
      //Obtiene los segundos acumulados desde unix y los desfaza segun la zona horaria.
-    uint segundosAcumEpoch = uint((int(block.timestamp) + (_timeZone*60*60)));
+    uint segundosAcumEpoch = uint((int(segundosEpoch) + (_timeZone*60*60)));
 
     // obtiene los valores de Minutos, Hora, dias y años acomulados desde unix
     uint minutosAcumEpoch = (segundosAcumEpoch/60) ;
@@ -44,20 +48,46 @@ struct date {
 
 
 //Funcion que calcula la catnidad de dias bisiestos para una fecha
- function cuantosDiasBisiestos (int _timeZone) internal view returns(uint) {
+ function cuantosDiasBisiestos (uint year) public pure returns(uint) {
      // Varibla contador de dias bisiestos
     uint diasBi = 0;
 
-    // Obtiene el año solicitado de la funcion fechaGet
-    uint year = fechaGet(_timeZone)[4];
 
     //Evalua para cada año desde epoch hasta el año solicitado la catnidad de años bisiestos y los cuenta
      for( uint c = 1970 ; c < year ; c++)
-     {if (c % 4 == 0 && (c % 100 != 0 || c % 400 == 0)){diasBi++ ;}}
+     {if ( esYearBisiesto( c )){diasBi++ ;}}
 
      //Devuelve la catidad de años bisiestos cotados
      return diasBi;
  }
+
+
+//Funcion para saber si un dia es bisiesto
+
+    function esYearBisiesto (uint year) internal pure returns(bool){
+
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+        {return true;} 
+        else {return false;}
+
+    }
+
+// Funcion que devuelve una fehca arreglada
+
+function fechaGet (uint seg) public view returns(uint[5] memory){
+    
+    uint[5] memory fecha = fechaNoFixGet( timeZone , seg);
+
+    fecha[3] = fecha[3] - cuantosDiasBisiestos( fecha[4] ) ;
+
+    if ( esYearBisiesto(fecha[4]) ) {
+        fecha[3] --;
+    } 
+
+    return fecha;
+
+}
+
 
 
  }
