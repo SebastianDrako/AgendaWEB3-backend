d24443c6ce9445ce8919192e83802a25351e1a49
SebasitanDrako <sguerrero.sgc@gmail.com>
2022-12-12 02:10:16 -0500

    Se re organizo la herencia entre contratos y se agregar el primer sistema para la gestion del almacenamiento de eventos basado en un indice por mes gregoriano acumulado desde epoch

---
+++ ContractTest.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./Agenda_Gestor.sol";
+
+contract test is agenda {
+
+ function t_
+
+}
---
+++ Opciones/Gestion_Del_tiempo.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./Basical_operations/Gestion_Del_Tiempo/Calendar.sol";
+
+contract GesTime is calendar {}
